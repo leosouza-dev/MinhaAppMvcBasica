4cc684ffee06ea6474595e373f3dd5861df86a96
Leonardo Rodrigues de Souza <sarival@sarival.com.br>
2020-01-24 17:50:24 -0300

    Modelando banco de dados com DataAnnotations e criando migration para tabelas do sistema

--- MinhaAppMvcBasica/Models/Endereco.cs
+++ MinhaAppMvcBasica/Models/Endereco.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MinhaAppMvcBasica.Models
 {
@@ -9,12 +7,31 @@ namespace MinhaAppMvcBasica.Models
     {
         //Chave estrangeira - FK  (este endereco pertence à um fornecedor
         public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Numero { get; set; }
+
         public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(8, ErrorMessage = "O campo {0} deve ter {1} caracteres", MinimumLength = 8)]
         public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Estado { get; set; }
 
         /* EF Relations */
--- MinhaAppMvcBasica/Models/Produto.cs
+++ MinhaAppMvcBasica/Models/Produto.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MinhaAppMvcBasica.Models
 {
@@ -9,11 +8,25 @@ namespace MinhaAppMvcBasica.Models
     {
         //Chave estrangeira - FK  (este produto pertence à um fornecedor
         public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1000, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Valor { get; set; }
+
+        [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
+
         public DateTime DataCadastro { get; set; }
 
         /* EF Relations */
